8996bb0f05629a7f01d797978de191bf445f809b
sindisil <greg.jandl@gmail.com>
2023-02-05 10:58:58 -0600

    First run at cmd line args, jusing clap
    Kinda think this is too heavy weight.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::path;
+
+use clap::Parser;
+use wild;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct CmdArgs {
+    /// Specifies the name of a file to read.
+    files: Vec<path::PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = wild::args();
+    let cmd_args = CmdArgs::parse_from(args);
+    println!("files: {:?}", cmd_args.files);
 }
