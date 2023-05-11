d17fe7a378c3ba3286170f39ff27b016f5e1a682
sindisil <greg.jandl@gmail.com>
2023-05-11 12:01:16 -0500

    Handle missing EOLs in read()
    Need to add in missing last buffer EOL when appending lines,
    and need to add in missing last EOL in read lines when
    inserting.
    
    In order to do this, we need to know what EOL char to
    insert. Hopefully it'll be consistent (?!), and it could
    probably be a config attribute eventually (with would then
    suggest additional EOL normalization as features). In the
    absense of a config item, and to better adapt to existing
    files, the code will assume the most used EOL char is the
    expected default.

--- src/edit_buffer.rs
+++ src/edit_buffer.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::fmt;
 use std::io::{self, prelude::*};
 use std::path;
@@ -7,6 +8,7 @@ pub struct EditBuffer {
     needs_write: bool,
     cur_line: usize,
     default_filename: Option<path::PathBuf>,
+    default_eol: Option<String>,
 }
 
 #[derive(Debug)]
@@ -49,6 +51,7 @@ impl EditBuffer {
             needs_write: false,
             cur_line: 0,
             default_filename: None,
+            default_eol: None,
         }
     }
 
@@ -83,12 +86,30 @@ impl EditBuffer {
         self.text.len()
     }
 
+    pub fn is_empty(&self) -> bool {
+        self.text.is_empty()
+    }
+
     /// Returns true if buffer has been changed since last write.
     pub fn needs_write(&self) -> bool {
         self.needs_write
     }
 
-    /// Reads lines from reader and appends them after the specified line.
+    /// Reads lines from reader into the buffer at the specified line.
+    ///
+    /// Default EOL auto-detect:
+    ///     If this call to read is on a buffer that has no default EOL, then new lines
+    ///     read are examined, and the default is set to the most frequently used EOL
+    ///     sequence.
+    ///
+    /// EOL Correction:
+    ///    If the final line read lacks an EOL, it will not be corrected
+    ///    if it is the last line of the buffer. Otherwise missing EOLs
+    ///    will be added. Added EOLs will be the default EOL for the
+    ///    buffer. This is determined either by configuration, or auto-detected
+    ///    (e.g., as described above, or similarly when first lines are appended
+    ///    or inserted).
+    ///
     /// Returns index of last line read, or an error if read fails
     pub fn read<R>(&mut self, at_line: usize, mut reader: R) -> Result<usize, Error>
     where
@@ -108,8 +129,58 @@ impl EditBuffer {
             line = String::new();
         }
         let lines_added = lines.len();
+
+        // set default_eol if neccessary
+        if self.default_eol.is_none() {
+            self.default_eol = Some(find_default_eol(&lines[..]));
+        }
+
+        // Add in missing eol as needed
+        let default_eol = self
+            .default_eol
+            .as_ref()
+            .expect("default_eol should have been set");
+        if !self.is_empty() {
+            let last_line = if at_line == self.len() {
+                let i = self.len() - 1;
+                &mut self.text[i]
+            } else {
+                let i = lines.len() - 1;
+                &mut lines[i]
+            };
+            let eol_pat = Regex::new(r"^.*(\r\n|\n|\r)$").unwrap();
+            if !eol_pat.is_match(last_line) {
+                last_line.push_str(default_eol.as_ref());
+            }
+        }
+
+        // actually add new lines to buffer
         self.text.splice(at_line..at_line, lines.into_iter());
         Ok(at_line + lines_added - 1)
+    }
+}
+
+fn find_default_eol(lines: &[String]) -> String {
+    let mut eols = vec![("\r\n", 0), ("\n", 0), ("\r", 0)];
+
+    for line in lines {
+        for eol in &mut eols {
+            if line.ends_with(eol.0) {
+                eol.1 += 1;
+                break;
+            }
+        }
+    }
+
+    eols.sort_by(|(_, a), (_, b)| b.cmp(a));
+    if eols[0].1 > 0 {
+        eols[0].0.to_string()
+    } else {
+        if std::env::consts::FAMILY == "windows" {
+            "\r\n".to_string()
+        } else {
+            "\n".to_string()
+        }
     }
 }
 
@@ -181,26 +252,57 @@ mod tests {
         assert_eq!(content, buffer.text);
         assert_eq!(3, last_line_read);
         assert_eq!(content.len(), buffer.len());
+        assert_eq!(buffer.default_eol, Some("\n".to_string()));
     }
 
     #[test]
     fn read_append() {
         let mut buffer = EditBuffer::new();
-        let content = vec![
+
+        let initial_content = vec!["Line1\n", "Line2\n", "Line3\n"];
+        let input = new_input_buf(&initial_content[..]);
+        let _last_read = buffer
+            .read(0, &input[..])
+            .expect("Error reading initial_content");
+
+        let new_content = vec!["New1\n", "New2\n", "New3\n"];
+        let input = new_input_buf(&new_content[..]);
+        let index = buffer.len();
+        let last_read = buffer
+            .read(index, &input[..])
+            .expect("Error reading new_content");
+
+        let final_content = vec![
             "Line1\n", "Line2\n", "Line3\n", "New1\n", "New2\n", "New3\n",
         ];
+        assert_eq!(final_content, buffer.text);
+        assert_eq!(final_content.len(), buffer.len());
+        assert_eq!(index + new_content.len() - 1, last_read);
+        assert_eq!(buffer.default_eol, Some("\n".to_string()));
+    }
 
-        let input = new_input_buf(&content[..3]);
-        let last_read = buffer.read(0, &input[..]).expect("Error reading content");
+    #[test]
+    fn read_append_no_trailing_eol() {
+        let mut buffer = EditBuffer::new();
 
-        let input = new_input_buf(&content[3..]);
+        let initial_content = vec!["Line1\n", "Line2\n", "Line3"];
+        let input = new_input_buf(&initial_content[..]);
+        let _last_read = buffer
+            .read(0, &input[..])
+            .expect("Error reading initial_content");
+
+        let new_content = vec!["New1\n", "New2\n", "New3"];
+        let input = new_input_buf(&new_content[..]);
+        let index = buffer.len();
         let last_read = buffer
-            .read(buffer.len(), &input[..])
-            .expect("Error reading new content");
+            .read(index, &input[..])
+            .expect("Error reading new_content");
 
-        assert_eq!(content, buffer.text);
-        assert_eq!(content.len(), buffer.len());
-        assert_eq!(buffer.len() - 1, last_read);
+        let final_content = vec!["Line1\n", "Line2\n", "Line3\n", "New1\n", "New2\n", "New3"];
+        assert_eq!(final_content, buffer.text);
+        assert_eq!(final_content.len(), buffer.len());
+        assert_eq!(index + new_content.len() - 1, last_read);
+        assert_eq!(buffer.default_eol, Some("\n".to_string()));
     }
 
     #[test]
@@ -209,7 +311,7 @@ mod tests {
 
         let initial_content = vec!["Line1\n", "Line2\n", "Line3\n"];
         let input = new_input_buf(&initial_content[..]);
-        let last_read = buffer
+        let _last_read = buffer
             .read(0, &input[..])
             .expect("Error reading initial_content");
 
@@ -226,6 +328,7 @@ mod tests {
         assert_eq!(final_content, buffer.text);
         assert_eq!(final_content.len(), buffer.len());
         assert_eq!(index + new_content.len() - 1, last_read);
+        assert_eq!(buffer.default_eol, Some("\n".to_string()));
     }
 
     #[test]
